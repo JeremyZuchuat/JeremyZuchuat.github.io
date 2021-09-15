d763edf83b98f6357e8bec785682bd3aae18bd21
JeremyZuchuat <jeremy.zuchuat@gmail.com>
2021-09-16 00:05:40 +0200

    Update script.js

--- map_test/script.js
+++ map_test/script.js
@@ -120,8 +120,8 @@ async function getGPX(path) {
 
   // GPX
   console.log("GPX LEAFLET");
-  var gpx = ''; // URL to your GPX file or the GPX itself
-  new L.GPX(gpx, {async: true}).on('loaded', function(e) {
+  // var gpx = ''; // URL to your GPX file or the GPX itself
+  new L.GPX(data_gpx, {async: true}).on('loaded', function(e) {
     mymap.fitBounds(e.target.getBounds());
   }).addTo(mymap);
 }
